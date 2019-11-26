cc5ab0259481120c33917b5eac5f713bb051fe29
khyassine <kh.yassine18@gmail.com>
2019-11-26 17:53:31 +0100

    add interpreter service interface

--- src/main/java/com/oracle/interpreter/service/InterpreterService.java
+++ src/main/java/com/oracle/interpreter/service/InterpreterService.java
@@ -1,5 +1,13 @@
 package com.oracle.interpreter.service;
 
+import com.oracle.interpreter.domain.Interpreter;
+import com.oracle.interpreter.domain.dto.InterpreterRequest;
+import com.oracle.interpreter.domain.dto.InterpreterResponse;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface InterpreterService {
 
+    InterpreterResponse execute(InterpreterRequest request);
+    Interpreter getInterpreter();
 }
--- src/main/java/com/oracle/interpreter/web/rest/InterpreterResource.java
+++ src/main/java/com/oracle/interpreter/web/rest/InterpreterResource.java
@@ -1,5 +1,6 @@
 package com.oracle.interpreter.web.rest;
 
+import com.oracle.interpreter.service.InterpreterService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/execute")
+
 public class InterpreterResource {
 
     private final Logger log = LoggerFactory.getLogger(InterpreterResource.class);
+    private final InterpreterService interpreterService;
 
+    public InterpreterResource(InterpreterService interpreterService) {
+        this.interpreterService = interpreterService;
+    }
 }
