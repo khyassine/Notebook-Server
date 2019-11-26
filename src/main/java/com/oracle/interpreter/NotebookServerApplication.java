710aa9552f887b210fff618ca97167ed10c022f4
khyassine <kh.yassine18@gmail.com>
2019-11-26 16:55:16 +0100

    initial commit

---
+++ src/main/java/com/oracle/interpreter/NotebookServerApplication.java
@@ -0,0 +1,13 @@
+package com.oracle.interpreter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotebookServerApplication.class, args);
+	}
+
+}
